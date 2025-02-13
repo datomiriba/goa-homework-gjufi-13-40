775d28a1ef9851725743a5953950823becfec414
datomiriba <datomiribani@gmail.com>
2025-02-13 11:00:58 -0800

    commit

---
+++ day 66/clsswork/classwork.py
@@ -0,0 +1,7 @@
+def integers_to_string(int_list):
+    str_list = [str(i) for i in int_list] 
+    print(" ".join(str_list))  
+
+integers_to_string([12,13,1333,13,1,31,13,1,3,13,1,31,4,465,45,45,34,35,3,4,35,35,5, ])
+
+
